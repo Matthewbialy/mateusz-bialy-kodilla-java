7b4467b6617e394cfb439680f42f2b6e574576d8
Mateusz Bialy <Matys@Air-Mateusz.home>
2019-01-05 17:23:25 +0100

    Stream 7.6

--- kodilla-exception-new/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception-new/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,12 +1,13 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
 
 public class ExceptionModuleRunner {
 
     public static void main(String args[]) {
 
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
+        fileReaderWithoutHandling.readFile();
     }
 }
