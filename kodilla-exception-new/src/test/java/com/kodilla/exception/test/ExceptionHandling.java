a95b3f0407e9d3f822c352d966e41870a1ce779d
Mateusz Bialy <Matys@Air-Mateusz.home>
2019-01-05 18:19:12 +0100

    First Challenge

--- kodilla-exception-new/src/test/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception-new/src/test/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,18 @@
 package com.kodilla.exception.test;
 
+
 public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(3.0, 3.0);
+        }catch (Exception e) {
+            System.out.println("problem has been gone...");
+        }finally {
+            System.out.println("Try - Catch - Finally - Succesfull");
+        }
+    }
 }
