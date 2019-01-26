48c3338eba2e4f48eb0fff404126f165058140df
Mateusz Bialy <Matys@Air-Mateusz.home>
2019-01-26 19:08:45 +0100

    Patterns Logger

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singelton/LoggerTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns.singelton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+@BeforeClass
+    public static void logSettingFile() {
+    Logger.getInstance().log("myapp.settings");
+}
+@Test
+    public void testGetLastLog() {
+    //Given
+    //Then
+    String lastLog = Logger.getInstance().getLastLog();
+    System.out.println("Last Log:" + lastLog);
+    //Then
+    Assert.assertEquals("myapp.settings", lastLog);
+}
+}
